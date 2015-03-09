23e9a135fc654180b2426d33e44ff38e63c9a0ec
Albert <albnok@hotmail.com>
2015-03-10 01:42:29 +0800

    Add backing datamap.png that contains four zones; detect if color in datamap.png has 0 green and look up blue in DUN hashmap; add hack to stop filling after arbitrary number of pixels

--- app/src/main/java/albert/test/bitmap/BitMapActivity.java
+++ app/src/main/java/albert/test/bitmap/BitMapActivity.java
@@ -4,28 +4,35 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import java.util.HashMap;
+
 
 public class BitMapActivity extends Activity {
     ImageView imageViewBitMap;
-    Bitmap bitmap;
+    Bitmap dataBitMap, dirtyBitMap;
     RelativeLayout relativeLayoutBitMap;
     float imageX, imageY;
-    int[] viewCoords = new int[2];
+    int[] viewCoordinates = new int[2];
+    HashMap<Integer, DUN> hashMap;
+    /** arbitrary number based on the biggest area's pixel count to optimize fill rates */
+    static final int MAX_FILL = 131072;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bit_map);
+        hashMap = new HashMap<>();
+        hashMap.put(0, new DUN("ZONE A Yellow", 334, Color.YELLOW));
+        hashMap.put(1, new DUN("ZONE B Green", 544, Color.GREEN));
+        hashMap.put(2, new DUN("ZONE C Blue", 671, Color.BLUE));
+        hashMap.put(3, new DUN("ZONE D Red", 121, Color.RED));
+
         imageViewBitMap = (ImageView) findViewById(R.id.imageViewBitMap);
         imageViewBitMap.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -35,25 +42,80 @@ public class BitMapActivity extends Activity {
             }
         });
         relativeLayoutBitMap = (RelativeLayout) findViewById(R.id.relativelayoutBitMap);
-        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.test);
-        imageViewBitMap.getLocationOnScreen(viewCoords);
-        imageViewBitMap.setImageBitmap(bitmap);
+        dataBitMap = BitmapFactory.decodeResource(getResources(), R.drawable.testmap);
+        dirtyBitMap = dataBitMap.copy(dataBitMap.getConfig(), true); // copies a fake map without the true data
+        dataBitMap = BitmapFactory.decodeResource(getResources(), R.drawable.datamap);
+        imageViewBitMap.getLocationOnScreen(viewCoordinates);
+        imageViewBitMap.setImageBitmap(dirtyBitMap);
+    }
+
+    class DUN {
+        String name;
+        int voters, color;
+
+        DUN(String name, int voters, int color) {
+            this.name = name;
+            this.voters = voters;
+            this.color = color;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getVoters() {
+            return voters;
+        }
+
+        public int getColor() {
+            return color;
+        }
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        imageY = bitmap.getHeight() / imageViewBitMap.getHeight();
-        imageX = bitmap.getWidth() / imageViewBitMap.getWidth();
+        imageY = dataBitMap.getHeight() / imageViewBitMap.getHeight();
+        imageX = dataBitMap.getWidth() / imageViewBitMap.getWidth();
     }
 
     void onTouched(MotionEvent motionEvent) {
-        float x = motionEvent.getX() - viewCoords[0];
-        float y = motionEvent.getY() - viewCoords[1];
-        int color = bitmap.getPixel((int) (x * imageX) , (int) (y * imageY));
-        relativeLayoutBitMap.setBackgroundColor(color);
-//        setTitle(x + ", " + y + " of iv: " + imageX + "x" + imageY);
-//        Log.d("BitMapAct.onTouched 3", x + ", " + y + " is " + Color.red(color) + "-" + Color.green(color) + "-" + Color.blue(color));
+        float x = motionEvent.getX() - viewCoordinates[0];
+        float y = motionEvent.getY() - viewCoordinates[1];
+        resolveMap((int) (x * imageX), (int) (y * imageY));
+    }
+
+
+    void resolveMap(int x, int y) {
+        int colorFound = dataBitMap.getPixel(x, y);
+        if (Color.green(colorFound)>0) {
+            return;
+        }
+        int blue = Color.blue(colorFound);
+        DUN dun = hashMap.get(blue);
+        if (dun==null) {
+            return;
+        }
+        int i = 0;
+        Log.d("BMA.resolveMap 2", "got something to look for... " + dun.getName());
+        for (int lx = 0; lx < dataBitMap.getWidth(); lx++) {
+            for (int ly = 0; ly < dataBitMap.getHeight(); ly++) {
+                if (dataBitMap.getPixel(lx, ly) == colorFound) {
+                    i++;
+                    if (i>MAX_FILL) {
+                        break;
+                    }
+                    dirtyBitMap.setPixel(lx, ly, dun.getColor());
+                }
+            }
+            if (i>MAX_FILL) {
+                break;
+            }
+        }
+        Log.d("BMA.resolveMap 3", "done looking for... " + dun.getName());
+        setTitle("Clicked " + dun.getName() + " pixels: " + i);
+
+        imageViewBitMap.setImageBitmap(dirtyBitMap);
     }
 
 }
